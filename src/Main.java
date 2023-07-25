cff12380fe2c59952d6fd50dc236aa71d6afaadc
marco <pradamarco21@gmail.com>
2023-07-25 16:40:44 -0500

    agregué el main

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,21 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Random;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Random random = new Random();
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        int amount;
+
+        System.out.print("Ingrese el valor de números a ingresar N: ");
+        try {
+            amount = Integer.parseInt(bufferedReader.readLine());
+        } catch (IOException exception) {
+            throw new RuntimeException("Debes ingresar un número entero x_x. ERROR!!!!");
+        }
+
+
     }
 }
